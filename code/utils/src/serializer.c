1fff8ec60485f5a655db8a7be0875dd6f25c965d
chris lee <chrislee7202@gmail.com>
2024-02-12 01:38:00 -0500

    mockup serialization

--- code/utils/src/serializer.c
+++ code/utils/src/serializer.c
@@ -8,10 +8,17 @@ static void fprintstr(FILE* restrict fp, const char* key, const char* data, bool
 static void fprintfloat(FILE* restrict fp, const char* key, double data, bool last);
 static const char *safestr(const char* str);
 
-static const char *chrus_node_type_to_str(enum CHRUS_NODE_TYPES);
+//static const char *chrus_node_type_to_str(enum CHRUS_NODE_TYPES);
 
 static void serialize_and_enqueue_children(ALLEGRO_EVENT_SOURCE* restrict event_source, FILE* restrict fp, chrus_node_vec* children);
 static void serialize_node(ALLEGRO_EVENT_SOURCE* restrict event_source, FILE* restrict fp, chrus_node* restrict node);
+
+static inline void serialize_camera(FILE* restrict fp, chrus_node* restrict node);
+static inline void serialize_script(FILE* restrict fp, chrus_node* restrict node);
+static inline void serialize_sprite(FILE* restrict fp, chrus_node* restrict node);
+static inline void serialize_sound(FILE* restrict fp, chrus_node* restrict node);
+static inline void serialize_audiostream(FILE* restrict fp, chrus_node* restrict node);
+static inline void serialize_text(FILE* restrict fp, chrus_node* restrict node);
 
 static inline void fprintspaces(FILE* restrict fp, int tabs) {
     fprintf(fp, "%*c", tabs * 2, ' ');
@@ -44,7 +51,7 @@ static void fprintstr(FILE* restrict fp, const char* key, const char* data, bool
 
 static void fprintfloat(FILE* restrict fp, const char* key, double data, bool last) {
     fprintspaces(fp, 1);
-    fprintf(fp, "\"%s\": \"%f\"", key, data);
+    fprintf(fp, "\"%s\": %f", key, data);
     fprintnewfield(fp, last);
 }
 
@@ -54,6 +61,7 @@ static const char *safestr(const char* str) {
     return str;
 }
 
+/*
 static const char *chrus_node_type_to_str(enum CHRUS_NODE_TYPES type) {
     switch (type)
     {
@@ -72,6 +80,7 @@ static const char *chrus_node_type_to_str(enum CHRUS_NODE_TYPES type) {
     }
     return "invalid";
 }
+*/
 
 static void serialize_and_enqueue_children(ALLEGRO_EVENT_SOURCE* restrict event_source, FILE* restrict fp, chrus_node_vec* children) {
     ALLEGRO_EVENT e;
@@ -81,14 +90,13 @@ static void serialize_and_enqueue_children(ALLEGRO_EVENT_SOURCE* restrict event_
     fprintf(fp, "\"children\": [\n");
     if (children->size > 0) {
         for (int i = 0; i < children->size-1; i++) {
-            printf("%p\n", children->data[i]);
-            e.user.data1 = children->data[i];
+            e.user.data1 = (intptr_t)children->data[i];
             al_emit_user_event(event_source, &e, NULL);
-            fprintf(fp, "    \"%p\",\n", children->data[i]);
+            fprintf(fp, "    \"%p\",\n", (void*)children->data[i]);
         }
-        e.user.data1 = children->data[children->size-1];
+        e.user.data1 = (intptr_t)children->data[children->size-1];
         al_emit_user_event(event_source, &e, NULL);
-        fprintf(fp, "    \"%p\"\n");
+        fprintf(fp, "    \"%p\"\n", (void*)children->data[children->size-1]);
     }
     fprintspaces(fp, 1);
     fprintf(fp, "]\n");
@@ -100,8 +108,100 @@ static void serialize_node(ALLEGRO_EVENT_SOURCE* restrict event_source, FILE* re
     fprintptr(fp, "id", node, false);
     fprintstr(fp, "name", node->name, false);
     fprintptr(fp, "parent", node->parent, false);
+    switch (node->type)
+    {
+    case CHRUS_NODE_UNINITIALIZED:
+        /* Huh? */
+        break;
+    case CHRUS_NODE_CAMERA:
+        serialize_camera(fp, node);
+        break;
+    case CHRUS_NODE_SCRIPT:
+        serialize_script(fp, node);
+        break;
+    case CHRUS_NODE_SPRITE:
+        serialize_sprite(fp, node);
+        break;
+    case CHRUS_NODE_SOUND:
+        serialize_sound(fp, node);
+        break;
+    case CHRUS_NODE_AUDIOSTREAM:
+        serialize_audiostream(fp, node);
+        break;
+    case CHRUS_NODE_TEXT:
+        serialize_text(fp, node);
+        break;
+    default:
+        break;
+    }
     /* implicitly enqueues children */
     serialize_and_enqueue_children(event_source, fp, &node->children);
+}
+
+static inline void serialize_camera(FILE* restrict fp, chrus_node* restrict node) {
+    chrus_camera camera = *(chrus_camera*)(node->data);
+
+    fprintfloat(fp, "sx", camera.screen_x, false);
+    fprintfloat(fp, "sy", camera.screen_y, false);
+    fprintfloat(fp, "sw", camera.screen_width, false);
+    fprintfloat(fp, "sh", camera.screen_height, false);
+    fprintfloat(fp, "vx", camera.viewport_x, false);
+    fprintfloat(fp, "vy", camera.viewport_y, false);
+    fprintfloat(fp, "vw", camera.viewport_width, false);
+    fprintfloat(fp, "vh", camera.viewport_height, false);
+}
+
+static inline void serialize_script(FILE* restrict fp, chrus_node* restrict node) {
+    chrus_script script = *(chrus_script*)(node->data);
+
+    fprintstr(fp, "source", script.source_name, false);
+}
+
+static inline void serialize_sprite(FILE* restrict fp, chrus_node* restrict node) {
+    chrus_sprite sprite = *(chrus_sprite*)(node->data);
+
+    fprintstr(fp, "source", sprite.source, false);
+    fprintfloat(fp, "x", sprite.x, false);
+    fprintfloat(fp, "y", sprite.y, false);
+    // width and height are calculated values...
+    fprintint(fp, "flipping", sprite.flipping, false);
+    fprintfloat(fp, "sx", sprite.sx, false);
+    fprintfloat(fp, "sy", sprite.sy, false);
+    fprintfloat(fp, "rotation", sprite.rotation, false);
+    fprintint(fp, "visible", sprite.visible, false);
+}
+
+static inline void serialize_sound(FILE* restrict fp, chrus_node* restrict node) {
+    chrus_sound sound = *(chrus_sound*)(node->data);
+    fprintstr(fp, "source", sound.source, false);
+    // length is a calculated value fprintfloat()
+    fprintint(fp, "playmode", sound.playmode, false);
+    fprintfloat(fp, "gain", sound.gain, false);
+    fprintfloat(fp, "pan", sound.pan, false);
+    fprintfloat(fp, "speed", sound.speed, false);
+}
+
+static inline void serialize_audiostream(FILE* restrict fp, chrus_node* restrict node) {
+    chrus_audiostream sound = *(chrus_audiostream*)(node->data);
+    fprintstr(fp, "source", sound.source, false);
+    // length is a calculated value fprintfloat()
+    fprintint(fp, "playmode", sound.playmode, false);
+    fprintfloat(fp, "gain", sound.gain, false);
+    fprintfloat(fp, "pan", sound.pan, false);
+    fprintfloat(fp, "speed", sound.speed, false);
+}
+
+static inline void serialize_text(FILE* restrict fp, chrus_node* restrict node) {
+    chrus_text text = *(chrus_text*)(node->data);
+    
+    /* we probably need a font object... */
+    /* todo: serialize color? */
+    fprintfloat(fp, "x", text.x, false);
+    fprintfloat(fp, "y", text.y, false);
+    fprintfloat(fp, "max_width", text.max_width, false);
+    fprintfloat(fp, "line_height", text.line_height, false);
+    fprintint(fp, "flags", text.flags, false);
+    fprintstr(fp, "text", text.text, false);
 }
 
 void chrus_serializer_save_scene(chrus_scene* scene, const char* filename) {
@@ -125,10 +225,10 @@ void chrus_serializer_save_scene(chrus_scene* scene, const char* filename) {
 
     /* this implementation is breadth-first search :cry: */
     while (al_get_next_event(scene_queue, &current_event)) {
-        chrus_node* current_node = current_event.user.data1;
+        chrus_node* current_node = (chrus_node*)current_event.user.data1;
 
         /* this function implicitly queues more nodes */
-        serialize_node(&scene_queue, fp, current_node);
+        serialize_node(&node_saver, fp, current_node);
         fputc('}', fp);
         if (!al_is_event_queue_empty(scene_queue)) {
             fputc(',', fp);
@@ -145,65 +245,5 @@ void chrus_serializer_save_scene(chrus_scene* scene, const char* filename) {
     al_destroy_event_queue(scene_queue);
 
     // for now, we can deserialize what we just serialized
-    chrus_serializer_deserialize_scene(filename);
-}
-
-static int get_key_and_value_type(json_stream* stream, const char* keybuffer, size_t* keylength, enum json_type* valuetype) {
-    *valuetype = json_next(&stream);
-    if (*valuetype == JSON_OBJECT_END) return CHRUS_PARSER_OBJECT_ENDED;
-    if (*valuetype != JSON_STRING) return CHRUS_PARSER_ERROR;
-    
-    keybuffer = json_get_string(&stream, keylength);
-    *valuetype = json_next(&stream);
-
-    return 0;
-}
-
-/* deserialization:
-    first pass through json: allocate ALL of the nodes
-    second pass through json: create the connections!!
-*/
-chrus_scene* chrus_serializer_deserialize_scene(const char* filename) {
-    FILE* fp = fopen(filename, "r");
-    if (!fp) return NULL;
-
-    printf("successfully opened scene file\n");
-
-    json_stream first_pass_stream;
-    enum json_type current_type;
-    chrus_scene* final_scene = NULL;
-    chrus_vector nodes = chrus_vector_create();
-
-    size_t field_length;
-    const char* str_buffer;
-    double number_buffer;
-
-    json_open_stream(&first_pass_stream, fp);
-
-    // let's make sure we're reading a json array then a json object
-    // we need to do some assertions too...
-    current_type = json_next(&first_pass_stream);
-    if (current_type != JSON_ARRAY) goto failure;
-    /* reading the scene object now */
-    current_type = json_next(&first_pass_stream);
-    if (current_type != JSON_OBJECT) goto failure;
-    current_type = json_next(&first_pass_stream);
-    if (current_type != JSON_STRING) goto failure;
-
-    while (get_key_and_value_type(&first_pass_stream, str_buffer, &field_length, &current_type) == 0) {
-        
-    }
-    str_buffer = json_get_string(&first_pass_stream, &field_length);
-    printf("scene field: %s\n", str_buffer);
-    current_type = json_next(&first_pass_stream);
-    number_buffer = json_get_number(&first_pass_stream);
-    printf("scene type: %d\n", (int)number_buffer);
-    goto success;
-
-    failure:
-    printf("experienced a failure\n");
-    if (final_scene) chrus_scene_destroy(final_scene);
-    return NULL;
-    success:
-    return final_scene;
+    //chrus_serializer_deserialize_scene(filename);
 }
